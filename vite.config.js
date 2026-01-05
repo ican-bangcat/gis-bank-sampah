4dd03ede152da805bdaf9032d8290c7cd937cf0d
M. Ikhsan Kurniawan <ikhsan23ti@mahasiswa.pcr.ac.id>
2026-01-05 14:27:37 +0700

    Update perbaikan deploy peta dan tabel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // PENTING: Ganti nama repo ini sesuai dengan nama repository GitHub kamu
+  // Kalau nama repo kamu 'gis-bank-sampah', biarkan seperti ini.
+  base: '/gis-bank-sampah/', 
+  
   build: {
+    // Ini fitur lama kamu (tetap kita simpan)
     sourcemap: true,
-  }
-}
+
+    // Tambahan biar terminal tidak bawel soal ukuran file
+    chunkSizeWarningLimit: 1600,
+  },
+})
